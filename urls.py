2f0fa0a545b418862b8b7958f80477b30f69fe38
Song Liu <leopku@qq.com>
2011-06-14 18:22:59 +0800

    fixed forgotten import login and logout in urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
